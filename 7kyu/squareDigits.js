e42478188f20ad5ebe5448edc7adada576ba45df
MaximOEgorov <llkk67@gmail.com>
2023-11-15 12:40:45 +0300

    commit 5

---
+++ 7kyu/squareDigits.js
@@ -0,0 +1,20 @@
+
+/*
+you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out,
+because 92 is 81 and 12 is 1. (81-1-1-81)
+Example #2: An input of 765 will/should return 493625
+because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+Note: The function accepts an integer and returns an integer.
+*/
+
+function squareDigits(num){
+    let newArr = Array.from(String(num), Number);
+    var res = "";
+    for (let i=0; i<newArr.length; i++) {
+        res = res + String(newArr[i]*newArr[i]);
+    }
+    return Number(res);
+}
+
+console.log(squareDigits(123))
