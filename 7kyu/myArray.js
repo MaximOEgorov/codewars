121d32352b2573dde9f65aecee883131dcbb8995
MaximOEgorov <llkk67@gmail.com>
2023-11-19 22:09:52 +0300

    commit 17

--- 7kyu/myArray.js
+++ 7kyu/myArray.js
@@ -73,7 +73,7 @@ about the number of elements of the array is not a multiple of 3.
 
 Example:
 
-    threeInOne( [1,2,3]) should return [6]
+threeInOne( [1,2,3]) should return [6]
 threeInOne( [1,2,3,4,5,6]) should return [6,15]
 threeInOne( [1,2,3,4,5,6,7,8,9]) should return [6,15,24]
 threeInOne( [1,3,5,2,4,6,7,7,7]) should return [9,12,21]*/
@@ -92,3 +92,60 @@ return res
 }
 
 // console.log(threeInOne( [1,2,3,4,5,6,7,8,9]))
+
+/*
+Task
+Coding in function isolateIt. function accept 1 parameters arr, it's a string array.
+Your task is to put a character "|" into the middle of each element.
+
+If the string length is an even number, use the insert method.
+for example: "abcd" should became "ab|cd". "|" should be inserted between ab and cd.
+
+If the string length is an odd number, use the replacement method.
+for example: "abcde" should became "ab|de". Character c will be replaced by |.
+
+The original array should not be changed, you need to return a new array
+(if you use the map method, you do not need to worry about this).
+
+Example
+isolateIt(["abcd","efgh"]) should return ["ab|cd","ef|gh"]
+isolateIt(["abcde","fghij"]) should return ["ab|de","fg|ij"]
+isolateIt(["1234","56789"]) should return ["12|34","56|89"]
+a little hint: Flexible use of slice() Will make the work more simple.*/
+
+function isolateIt(arr){
+    return arr.map(x=>x.slice(0,Math.floor(x.length/2))+"|"+x.slice(Math.floor(x.length/2)+x.length%2))
+}
+
+// console.log(isolateIt(["abzde","fgzij"]))
+
+/*
+Task
+Coding in function countGrade. function accept 1 parameters scores, it's a number array.
+Your task is to count the grade distribution of the scores, to return an object like this:
+
+{S:888, A:888, B:888, C:888, D:888, X:888}
+Grading rules:
+
+Grade S: Full marks(score=100)
+Grade A: score<100 and score>=90
+Grade B: score<90 and score>=80
+Grade C: score<80 and score>=60
+Grade D: score<60 and score>=0
+Grade X: score=-1(The cheating guy gets a score like that)
+Example
+countGrade([50,60,70,80,90,100]) should return {S:1, A:1, B:1, C:2, D:1, X:0}
+countGrade([65,75,,85,85,95,100,100]) should return {S:2, A:1, B:2, C:2, D:0, X:0}
+countGrade([-1,-1,-1,-1,-1,-1]) should return {S:0, A:0, B:0, C:0, D:0, X:6}*/
+
+function countGrade(scores){
+   let s = scores.filter(x=>(x==100)).length
+   let a = scores.filter(x=>(x<100 && x>=90)).length
+   let b = scores.filter(x=>(x<90 && x>=80)).length
+   let c = scores.filter(x=>(x<80 && x>=60)).length
+   let d = scores.filter(x=>(x<60 && x>=0)).length
+   let x = scores.filter(x=>(x==-1)).length
+   return {S:s, A:a, B:b, C:c, D:d, X:x}
+}
+
+console.log(countGrade([65,75,,85,85,95,100,100]))
