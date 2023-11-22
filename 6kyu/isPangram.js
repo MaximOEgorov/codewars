4aed32ddfedda844ff4815158a7c3e8a0c3a0a7e
MaximOEgorov <llkk67@gmail.com>
2023-11-22 22:34:02 +0300

    commit 25

--- 6kyu/isPangram.js
+++ 6kyu/isPangram.js
@@ -8,4 +8,18 @@ because it uses the letters A-Z at least once (case is irrelevant).
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
 Ignore numbers and punctuation.*/
 
+function isPangram(string){
+    return 'abcdefghijklmnopqrstuvwxyz'
+        .split('')
+        .every((x) => string.toLowerCase().includes(x));
+}
+function isPangram__(string){
+    let a = string.split("")
+    let z = new Array(91)
+    debugger
+    a.map(x => {z[x.toUpperCase().charCodeAt()] = 0})
+    return  !z.slice(65, 90).includes(undefined);
+}
 
+//var string = "aaaaaaaaaaaaaaaaaaaaaaaaa"
+//console.log(isPangram(string))
