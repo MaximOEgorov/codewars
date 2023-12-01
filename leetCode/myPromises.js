a3b5be048097378dfbdc7cf998df543136c9598f
MaximOEgorov <llkk67@gmail.com>
2023-12-01 21:41:39 +0300

    commit 27

--- leetCode/myPromises.js
+++ leetCode/myPromises.js
@@ -15,5 +15,17 @@ var addTwoPromises = async function(promise1, promise2) {
 };
 
 
- addTwoPromises(Promise.resolve(2), Promise.resolve(2))
-   .then(console.log); // 4
+/* addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+   .then(console.log); // 4*/
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
